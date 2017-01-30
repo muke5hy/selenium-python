b2431be323f64c51163981922f7ecbc06f16c928
Mukesh Yadav <mak.gnu@gmail.com>
2017-01-30 18:04:00 +0530

    Added files

---
+++ rbc.py
@@ -0,0 +1,29 @@
+import os, time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+os.environ["PATH"] += ":/Users/mukeshyadav/Projects/rbcbank/"
+
+
+driver = webdriver.Firefox()
+driver.get("http://www.rbcbank.com/index.page")
+print driver.current_url
+driver.find_element_by_link_text("Sign In").click()
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "K1"))
+    )
+    print driver.current_url
+    username = driver.find_element_by_xpath('//*[@id="K1"]')
+    password = driver.find_element_by_xpath('//*[@id="Q1"]')
+
+    username.send_keys("YourUsername")
+    password.send_keys("Pa55worD")
+
+    driver.find_element_by_name("Sign In").click()
+
+finally:
+    driver.close()
