9b18c6ee019871eb0e0b3f67e5188ceac379d9a3
Mukesh Yadav <mak.gnu@gmail.com>
2017-02-08 22:25:58 +0530

    Added youtube and moved libs into file

---
+++ youtube/config-sample.py
@@ -0,0 +1,9 @@
+
+facebook_username = 'email'
+facebook_password = 'password'
+
+locations = {
+				'mumbai':'114759761873412',
+				'delhi':'106517799384578',
+				'kolkata':'108212225873404'
+			}
